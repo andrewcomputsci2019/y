471e462b5a77915a1656ad2f9c46373f8f5e6595
[Andrew <andrewcomputsci2019@gmail.com>
2019-10-29 15:06:28 -0700

    Initial Commit

---
+++ Y/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Y
+//
+//  Created by Pegg, Andrew on 10/29/19.
+//  Copyright © 2019 Pegg, Andrew. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
